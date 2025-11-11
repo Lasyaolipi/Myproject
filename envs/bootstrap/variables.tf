6b72a7c1a1a651f24ec53e93e92f90fb3cdc0b18
lasyaolipi <kumarolipi88@gmail.com>
2025-11-11 10:27:35 +0530

    updated variable file

--- envs/bootstrap/variables.tf
+++ envs/bootstrap/variables.tf
@@ -1,4 +1,4 @@
-variable "region" { type = string, default = "us-east-1" }
+variable "region" { type = string, default = "ap-south-1" }
 variable "state_bucket" { type = string }
 variable "dynamodb_table" { type = string }
 variable "log_bucket" { type = string }
