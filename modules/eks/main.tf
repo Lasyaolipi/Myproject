0bae1b84d0687094d28e1d563aec80d9552c3011
lasyaolipi <kumarolipi88@gmail.com>
2025-11-11 11:55:52 +0530

    Terraform validate

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,25 +6,33 @@ terraform {
     }
   }
 }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.0.0"
 
-  cluster_name = var.cluster_name
-  vpc_id       = var.vpc_id
-  private_subnets = var.private_subnet_ids
-  public_subnets  = var.public_subnet_ids
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  vpc_id          = var.vpc_id
+  subnet_ids      = concat(var.private_subnet_ids, var.public_subnet_ids)
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
       instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
     }
   }
+
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
 }
+
 
 output "cluster_endpoint" { value = module.eks.cluster_endpoint }
 output "cluster_name" { value = module.eks.cluster_id }
