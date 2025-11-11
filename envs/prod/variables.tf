74b28c0bed950b743e843177ab8ba6a1a9c34437
lasyaolipi <kumarolipi88@gmail.com>
2025-11-11 11:40:24 +0530

    code updated

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,3 +1,15 @@
-variable "region" {"type" = string, "default" = "ap-south-1" }
-variable "aws_account_id" { type = string, default = "391313099163" }
-variable "cluster_name" { type = string, default = "prod-eks" }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "aws_account_id" {
+  type    = string
+  default = "391313099163"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "prod-eks"
+}
+
